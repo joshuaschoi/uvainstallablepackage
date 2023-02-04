a03719ae1b030f5721267d71c24f3dedc75b0aa5
joshuaschoi <44579199+joshuaschoi@users.noreply.github.com>
2023-02-04 16:18:47 -0500

    Update test_shared.py

--- tests/test_shared.py
+++ tests/test_shared.py
@@ -6,3 +6,6 @@ def test_clean_string():
     test_str = " This! is      a ,test string  "
 
     assert "This is a test string" == sh.clean_string(test_str), "String <{}> not cleaned as expected".format(test_str)
+    
+def identifier():
+    print 'Hello World, This is Joshua Choi'
